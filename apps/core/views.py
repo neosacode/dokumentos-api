7ecf98db01db283025175ab056dc3a2822e40cd7
Juliano Gouveia <juliano@neosacode.com>
2018-03-16 17:08:02 -0300

    User auth and change model views for viewsets

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView
+from rest_framework import authentication, permissions
+from django.contrib.auth import get_user_model
+from apps.core import serializers
 
-# Create your views here.
+
+class CreateUserView(CreateAPIView):
+    model = get_user_model()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = serializers.CreateUserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication,)
+
+    def get_queryset(self):
+        return [self.request.user]
--- apps/documents/views.py
+++ apps/documents/views.py
@@ -1,25 +1,12 @@
-from apps.documents.serializers import DocumentsSerializer
+from apps.documents.serializers import DocumentSerializer
 from apps.documents.models import Document
-from rest_framework import generics
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from apps.documents.permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 
 
-class DocumentsList(generics.ListCreateAPIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
-    permission_classes = (IsAuthenticated,)
-    # queryset = Document.objects.all()
-    serializer_class = DocumentsSerializer
+class DocumentViewSet(viewsets.ModelViewSet):
+    serializer_class = DocumentSerializer
+    authentication_classes = (SessionAuthentication, TokenAuthentication,)
 
     def get_queryset(self):
-        queryset = Document.objects.filter(user=self.request.user)
-        return queryset
-
-
-class DocumentDetail(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
-    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
-    queryset = Document.objects.all()
-    
-    serializer_class = DocumentsSerializer
+        return Document.objects.filter(user=self.request.user)
