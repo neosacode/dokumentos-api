9c18f7d0d829846aaff9e2bc2bc1b07bbdf148ca
Juliano Gouveia <juliano@neosacode.com>
2018-03-16 16:05:59 -0300

    Remove save wrong set_password call and add in create user serializer

--- apps/documents/serializers.py
+++ apps/documents/serializers.py
@@ -6,3 +6,9 @@ class DocumentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = "__all__"
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
