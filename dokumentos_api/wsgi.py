8242ad364bd4296592074a7296ded4ae3fab20a4
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-03-21 19:49:14 -0300

    Change DJANGO_SETTINGS_MODULE env var place

--- dokumentos_api/wsgi.py
+++ dokumentos_api/wsgi.py
@@ -4,12 +4,13 @@ import psycogreen.gevent
 import gevent.monkey
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dokumentos_api.settings")
+
 RUNNING_DEVSERVER = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
 
 if not RUNNING_DEVSERVER:
     gevent.monkey.patch_all()
     psycogreen.gevent.patch_psycopg()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dokumentos_api.settings")
 
 application = get_wsgi_application()
