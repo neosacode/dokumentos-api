ec7b35b3194471250959d0d774e267aa35a89e46
Juliano Gouveia <juliano@neosacode.com>
2018-03-22 07:14:16 -0300

    Change wsgi.py monkey patching

--- dokumentos_api/wsgi.py
+++ dokumentos_api/wsgi.py
@@ -1,16 +1,16 @@
-import os
 import sys
-import psycogreen.gevent
-import gevent.monkey
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dokumentos_api.settings")
-
 RUNNING_DEVSERVER = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
 
 if not RUNNING_DEVSERVER:
-    psycogreen.gevent.patch_psycopg()
-    gevent.monkey.patch_all()
+	import gevent.monkey
+	gevent.monkey.patch_all()
+	import psycogreen.gevent
+	psycogreen.gevent.patch_psycopg()
 
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dokumentos_api.settings")
 
 application = get_wsgi_application()
