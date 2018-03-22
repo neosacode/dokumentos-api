8095f84843e9dfdd3384af271df6469b85556461
Juliano Gouveia <juliano@neosacode.com>
2018-03-22 08:04:43 -0300

    Making psycopg green

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,9 @@
 worker_class = 'gevent'
 worker_connections = 50
 workers = 2
+preload_app = True
 
 def pre_fork(server, worker):
     import psycogreen.gevent
     psycogreen.gevent.patch_psycopg()
+    print('Making psycopg green')
